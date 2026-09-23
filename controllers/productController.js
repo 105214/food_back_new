0bbc42ca512eb2145263978482569389f21dd23a
nithin <nithinsukumar200@gmail.com>
2026-09-23 19:38:49 +0530

    product created

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,8 +1,18 @@
+import Product from "../models/productModel"
 
 
 const addProduct = async(req,res)=>{
     try {
-        
+        const {name,price,image,incrediants,category,isAvailable,isVeg,stock} = req.body
+
+        if(!name || !price || !image || !incrediants || !category || !isAvailable || !isVeg || !stock){
+            return res.status(400).json({message:"All fields are required"})
+        }
+        const productExist = await Product.findOne({name})
+
+        if(!productExist){
+            return  res.status(404).json({message:"Product not found"})
+        }
     } catch (error) {
         
     }
--- models/productModel.js
+++ models/productModel.js
@@ -1,2 +1,49 @@
+import mongoose, { Schema } from "mongoose";
 
 
+const productSchema = new Schema({
+
+    name:{
+        type:String,
+        required:true,
+        trim:true
+
+    },
+    price:{
+        type:Number,
+        required:true,
+        min:0
+    },
+    incrediants:{
+          type:String,
+          required:true,
+          trim:true
+    },
+    image:{
+        type:String
+    },
+    category:{ 
+     type:String,
+     enum:['juice','cake'],
+     required:true
+    },
+    stock:{
+       type:Number,
+       default:0,
+       min:0
+    },
+    isAvailable:{
+        type:Boolean,
+        default:true
+    },
+    isVeg:{
+        type:Boolean,
+        default:true,
+        required:true
+    }
+
+})
+
+const Product = mongoose.model('Product',productSchema)
+
+export default Product
